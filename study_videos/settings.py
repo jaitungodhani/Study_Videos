3f8fb000b398e8e58def106553ed0ae4b4b5a016
jaitunservicepack <jaitun.godhani@servicepack.ai>
2023-06-02 16:42:36 +0530

    change

--- study_videos/settings.py
+++ study_videos/settings.py
@@ -28,8 +28,9 @@ SECRET_KEY = 'b3d*5$a(cbyvxu5$k6vj6br)x^7)7-vwmda^2(f5c597-&i@u^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ec2-18-234-239-21.compute-1.amazonaws.com", "18.234.239.21", "localhost," "127.0.0.1"]
+# ALLOWED_HOSTS = ["ec2-18-234-239-21.compute-1.amazonaws.com", "18.234.239.21", "localhost," "127.0.0.1"]
 
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
